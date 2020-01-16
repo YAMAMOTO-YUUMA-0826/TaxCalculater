e21258208ceb76c04ab09ebd6adca9b0e7fea50e
Yuuma Yamamoto <yuuma.uh1113@gmail.com>
2020-01-16 19:33:03 +0900

    Initial Commit

---
+++ TaxCulculateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxCulculateApp
+//
+//  Created by 山本悠磨 on 2020/01/16.
+//  Copyright © 2020 Yuuma Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
